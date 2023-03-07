6a7743c1d18d7087735cf3595b6ddff895df4e05
git-abadano <abadano05@gmail.com>
2023-03-06 23:21:36 -0300

    Ajuste de Api Key

--- src/constants/firebase/index.js
+++ src/constants/firebase/index.js
@@ -1,4 +1,4 @@
 export const FB_REALTIME_DB_URL = 'https://bazar5aapp-default-rtdb.firebaseio.com/'
-export const FB_API_KEY = 'AIzaSyBpHJow6H1Av8POYSfizjUcfj-WqZB6E4k'
+export const FB_API_KEY = ''
 export const FB_URL_AUTH_SIGN_UP = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${FB_API_KEY}`;
 export const FB_URL_AUTH_SIGN_IN = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${FB_API_KEY}`;
